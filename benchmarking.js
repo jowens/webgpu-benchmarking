d8ec60162f321d0b47dbe15c70522df2b0108fff
John Owens <jowens@ece.ucdavis.edu>
2024-10-16 15:47:22 -0700

    new generate-test-parameters structure

--- benchmarking.js
+++ benchmarking.js
@@ -1,9 +1,5 @@
 import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm";
-
-function fail(msg) {
-  // eslint-disable-next-line no-alert
-  alert(msg);
-}
+// util functions in util.js, imported by HTML wrapper
 
 const adapter = await navigator.gpu?.requestAdapter();
 const hasSubgroups = adapter.features.has("subgroups");
@@ -27,8 +23,10 @@ const membwTest = {
   type: "memory",
   description:
     "Copies input array to output array. One thread is assigned per 32b input element.",
-  workgroupSizes: range(0, 7).map((i) => 2 ** i),
-  memsrcSizes: range(10, 25).map((i) => 2 ** i),
+  parameters: {
+    workgroupSize: range(0, 7).map((i) => 2 ** i),
+    memsrcSize: range(10, 25).map((i) => 2 ** i),
+  },
   trials: 10,
   kernel: (workgroupSize) => /* wgsl */ `
     /* output */
@@ -53,7 +51,7 @@ const membwTest = {
   plot: {
     x: { x: "memsrcSize", label: "Copied array size (B)" },
     y: { y: "bandwidth", label: "Achieved bandwidth (GB/s)" },
-    stroke: {stroke: "workgroupSize"},
+    stroke: { stroke: "workgroupSize" },
   },
 };
 
@@ -62,10 +60,13 @@ const maddTest = {
   type: "compute",
   description:
     "Computes N multiply-adds per input element. One thread is responsible for one 32b input element.",
-  workgroupSizes: range(0, 7).map((i) => 2 ** i),
-  memsrcSizes: range(10, 26).map((i) => 2 ** i),
+  parameters: {
+    workgroupSize: range(0, 7).map((i) => 2 ** i),
+    memsrcSize: range(10, 26).map((i) => 2 ** i),
+  },
   trials: 10,
-  kernel: (workgroupSize, ops) => { /* wsgl */ var k = `
+  kernel: (workgroupSize, opsPerKernel) => {
+    /* wsgl */ var k = `
     /* output */
     @group(0) @binding(0) var<storage, read_write> memDest: array<f32>;
     /* input */
@@ -81,11 +82,11 @@ const maddTest = {
         /* 2^-22 = 2.38418579e-7 */
         var b = f * 2.38418579e-7 + 1.0;
         /* b is a float btwn 1 and 2 */`;
-    while (ops > 0) {
-      k = k + "f = f * b + b;\n"
-      ops -= 2;
+    while (opsPerKernel > 0) {
+      k = k + "    f = f * b + b;\n";
+      opsPerKernel -= 2;
     }
-    k = k + "memDest[i] = f;}}";
+    k = k + "    memDest[i] = f;\n}\n}";
     return k;
   },
   validate: (input, output) => {
@@ -100,7 +101,7 @@ const maddTest = {
     f = f * b + b;
     f = f * b + b;
     // allow for a bit of FP error
-    return (Math.abs(f - output) / f) < 0.00001;
+    return Math.abs(f - output) / f < 0.00001;
   },
   bytesTransferred: (memInput, memOutput) => {
     return memInput.byteLength + memOutput.byteLength;
@@ -117,9 +118,8 @@ const maddTest = {
   plot: {
     x: { x: "threadCount", label: "Active threads" },
     y: { y: "gflops", label: "GFLOPS" },
-    stroke: {stroke: "workgroupSize"},
+    stroke: { stroke: "workgroupSize" },
   },
-
 };
 
 const reducePerWGTest = {
@@ -157,164 +157,164 @@ const tests = [membwTest, maddTest];
 
 for (const test of tests) {
   const data = [];
-  for (const workgroupSize of test.workgroupSizes) {
-    for (const memsrcSize of test.memsrcSizes) {
-      const timingHelper = new TimingHelper(device);
+  for (const param of combinations(test.parameters)) {
+    const memsrcSize = param.memsrcSize;
+    const workgroupSize = param.workgroupSize;
+    const timingHelper = new TimingHelper(device);
 
-      const workgroupCount = memsrcSize / workgroupSize;
-      const dispatchGeometry = [workgroupCount, 1];
-      while (
-        dispatchGeometry[0] > adapter.limits.maxComputeWorkgroupsPerDimension
-      ) {
-        dispatchGeometry[0] /= 2;
-        dispatchGeometry[1] *= 2;
-      }
-      console.log(`workgroup count: ${workgroupCount}
+    const workgroupCount = memsrcSize / workgroupSize;
+    const dispatchGeometry = [workgroupCount, 1];
+    while (
+      dispatchGeometry[0] > adapter.limits.maxComputeWorkgroupsPerDimension
+    ) {
+      dispatchGeometry[0] /= 2;
+      dispatchGeometry[1] *= 2;
+    }
+    console.log(`workgroup count: ${workgroupCount}
       workgroup size: ${workgroupSize}
       maxComputeWGPerDim: ${adapter.limits.maxComputeWorkgroupsPerDimension}
       dispatchGeometry: ${dispatchGeometry}`);
 
-      const memsrc = new Float32Array(memsrcSize);
-      for (let i = 0; i < memsrc.length; i++) {
-        memsrc[i] = i & (2**22 - 1) // roughly, range of 32b significand
-      }
+    const memsrc = new Float32Array(memsrcSize);
+    for (let i = 0; i < memsrc.length; i++) {
+      memsrc[i] = i & (2 ** 22 - 1); // roughly, range of 32b significand
+    }
 
-      const memcpyModule = device.createShaderModule({
-        label: `module: ${test.name}`,
-        code: test.kernel(workgroupSize, 256),
-      });
+    const memcpyModule = device.createShaderModule({
+      label: `module: ${test.name}`,
+      code: test.kernel(workgroupSize, 256),
+    });
 
-      const kernelPipeline = device.createComputePipeline({
-        label: "memcpy compute pipeline",
-        layout: "auto",
-        compute: {
-          module: memcpyModule,
-        },
-      });
+    const kernelPipeline = device.createComputePipeline({
+      label: "memcpy compute pipeline",
+      layout: "auto",
+      compute: {
+        module: memcpyModule,
+      },
+    });
 
-      // create buffers on the GPU to hold data
-      // read-only inputs:
-      const memsrcBuffer = device.createBuffer({
-        label: "memory source buffer",
-        size: memsrc.byteLength,
-        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
-      });
-      device.queue.writeBuffer(memsrcBuffer, 0, memsrc);
+    // create buffers on the GPU to hold data
+    // read-only inputs:
+    const memsrcBuffer = device.createBuffer({
+      label: "memory source buffer",
+      size: memsrc.byteLength,
+      usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+    });
+    device.queue.writeBuffer(memsrcBuffer, 0, memsrc);
 
-      const memdestBuffer = device.createBuffer({
-        label: "memory destination buffer",
-        size: memsrc.byteLength,
-        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
-      });
+    const memdestBuffer = device.createBuffer({
+      label: "memory destination buffer",
+      size: memsrc.byteLength,
+      usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
+    });
 
-      const mappableMemdstBuffer = device.createBuffer({
-        label: "mappable memory destination buffer",
-        size: memsrc.byteLength,
-        usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
-      });
+    const mappableMemdstBuffer = device.createBuffer({
+      label: "mappable memory destination buffer",
+      size: memsrc.byteLength,
+      usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
+    });
 
-      /** Set up bindGroups per compute kernel to tell the shader which buffers to use */
-      const kernelBindGroup = device.createBindGroup({
-        label: "bindGroup for memcpy kernel",
-        layout: kernelPipeline.getBindGroupLayout(0),
-        entries: [
-          { binding: 0, resource: { buffer: memdestBuffer } },
-          { binding: 1, resource: { buffer: memsrcBuffer } },
-        ],
-      });
+    /** Set up bindGroups per compute kernel to tell the shader which buffers to use */
+    const kernelBindGroup = device.createBindGroup({
+      label: "bindGroup for memcpy kernel",
+      layout: kernelPipeline.getBindGroupLayout(0),
+      entries: [
+        { binding: 0, resource: { buffer: memdestBuffer } },
+        { binding: 1, resource: { buffer: memsrcBuffer } },
+      ],
+    });
 
-      const encoder = device.createCommandEncoder({
-        label: "kernel encoder",
-      });
-      /* run a bunch of kernels before we start timing, don't time overhead */
-      const kernelPrepass = encoder.beginComputePass(encoder, {
-        label: "untimed kernel compute pass",
-      });
-      kernelPrepass.setPipeline(kernelPipeline);
-      kernelPrepass.setBindGroup(0, kernelBindGroup);
-      for (var i = 0; i < test.trials; i++) {
-        kernelPrepass.dispatchWorkgroups(...dispatchGeometry);
-      }
-      kernelPrepass.end();
+    const encoder = device.createCommandEncoder({
+      label: "kernel encoder",
+    });
+    /* run a bunch of kernels before we start timing, don't time overhead */
+    const kernelPrepass = encoder.beginComputePass(encoder, {
+      label: "untimed kernel compute pass",
+    });
+    kernelPrepass.setPipeline(kernelPipeline);
+    kernelPrepass.setBindGroup(0, kernelBindGroup);
+    for (var i = 0; i < test.trials; i++) {
+      kernelPrepass.dispatchWorkgroups(...dispatchGeometry);
+    }
+    kernelPrepass.end();
 
-      const kernelPass = timingHelper.beginComputePass(encoder, {
-        label: "timed kernel compute pass",
-      });
-      kernelPass.setPipeline(kernelPipeline);
-      kernelPass.setBindGroup(0, kernelBindGroup);
-      // TODO handle not evenly divisible by wgSize
-      for (var i = 0; i < test.trials; i++) {
-        kernelPass.dispatchWorkgroups(...dispatchGeometry);
-      }
-      kernelPass.end();
+    const kernelPass = timingHelper.beginComputePass(encoder, {
+      label: "timed kernel compute pass",
+    });
+    kernelPass.setPipeline(kernelPipeline);
+    kernelPass.setBindGroup(0, kernelBindGroup);
+    // TODO handle not evenly divisible by wgSize
+    for (var i = 0; i < test.trials; i++) {
+      kernelPass.dispatchWorkgroups(...dispatchGeometry);
+    }
+    kernelPass.end();
 
-      // Encode a command to copy the results to a mappable buffer.
-      // this is (from, to)
-      encoder.copyBufferToBuffer(
-        memdestBuffer,
-        0,
-        mappableMemdstBuffer,
-        0,
-        mappableMemdstBuffer.size
-      );
+    // Encode a command to copy the results to a mappable buffer.
+    // this is (from, to)
+    encoder.copyBufferToBuffer(
+      memdestBuffer,
+      0,
+      mappableMemdstBuffer,
+      0,
+      mappableMemdstBuffer.size
+    );
 
-      // Finish encoding and submit the commands
-      const command_buffer = encoder.finish();
-      device.queue.submit([command_buffer]);
+    // Finish encoding and submit the commands
+    const command_buffer = encoder.finish();
+    device.queue.submit([command_buffer]);
 
-      // Read the results
-      await mappableMemdstBuffer.mapAsync(GPUMapMode.READ);
-      const memdest = new Float32Array(
-        mappableMemdstBuffer.getMappedRange().slice()
-      );
-      mappableMemdstBuffer.unmap();
-      let errors = 0;
-      for (let i = 0; i < memdest.length; i++) {
-        if (!test.validate(memsrc[i], memdest[i])) {
-          if (errors < 5) {
-            console.log(
-              `Error ${errors}: i=${i}, input=${memsrc[i]}, output=${
-                memdest[i]
-              }, expected=${test.validate(memsrc[i])}`
-            );
-          }
-          errors++;
-        }
-      }
-      if (errors > 0) {
-        console.log(`Memdest size: ${memdest.length} | Errors: ${errors}`);
-      } else {
-        console.log(`Memdest size: ${memdest.length} | No errors!`);
-      }
-
-      timingHelper.getResult().then((ns) => {
-        const result = {
-          name: test.name,
-          time: ns / test.trials,
-          workgroupSize: workgroupSize,
-          memsrcSize: memsrcSize,
-        };
-        if (test.bytesTransferred) {
-          result.bytesTransferred = test.bytesTransferred(memsrc, memdest);
-          result.bandwidth = result.bytesTransferred / result.time;
-        }
-        if (test.threadCount) {
-          result.threadCount = test.threadCount(memsrc);
-        }
-        if (test.flopsPerThread) {
-          result.flopsPerThread = test.flopsPerThread();
-        }
-        if (test.gflops && result.threadCount && result.flopsPerThread) {
-          result.gflops = test.gflops(
-            result.threadCount,
-            result.flopsPerThread,
-            result.time
+    // Read the results
+    await mappableMemdstBuffer.mapAsync(GPUMapMode.READ);
+    const memdest = new Float32Array(
+      mappableMemdstBuffer.getMappedRange().slice()
+    );
+    mappableMemdstBuffer.unmap();
+    let errors = 0;
+    for (let i = 0; i < memdest.length; i++) {
+      if (!test.validate(memsrc[i], memdest[i])) {
+        if (errors < 5) {
+          console.log(
+            `Error ${errors}: i=${i}, input=${memsrc[i]}, output=${
+              memdest[i]
+            }, expected=${test.validate(memsrc[i])}`
           );
         }
-        data.push(result);
-        console.log(result);
-      });
+        errors++;
+      }
     }
+    if (errors > 0) {
+      console.log(`Memdest size: ${memdest.length} | Errors: ${errors}`);
+    } else {
+      console.log(`Memdest size: ${memdest.length} | No errors!`);
+    }
+
+    timingHelper.getResult().then((ns) => {
+      const result = {
+        name: test.name,
+        time: ns / test.trials,
+        workgroupSize: workgroupSize,
+        memsrcSize: memsrcSize,
+      };
+      if (test.bytesTransferred) {
+        result.bytesTransferred = test.bytesTransferred(memsrc, memdest);
+        result.bandwidth = result.bytesTransferred / result.time;
+      }
+      if (test.threadCount) {
+        result.threadCount = test.threadCount(memsrc);
+      }
+      if (test.flopsPerThread) {
+        result.flopsPerThread = test.flopsPerThread();
+      }
+      if (test.gflops && result.threadCount && result.flopsPerThread) {
+        result.gflops = test.gflops(
+          result.threadCount,
+          result.flopsPerThread,
+          result.time
+        );
+      }
+      data.push(result);
+      console.log(result);
+    });
   }
   console.log(data);
 
@@ -332,7 +332,7 @@ for (const test of tests) {
           x: test.plot.x.x,
           y: test.plot.y.y,
           z: test.plot.stroke.stroke,
-          text: test.plot.stroke.stroke,          
+          text: test.plot.stroke.stroke,
           textAnchor: "start",
           dx: 3,
         })
---
+++ util.js
@@ -0,0 +1,30 @@
+// not thrilled this uses idx, would rather do destructuring and not rely on idx/length
+// would also like to see this done with a generatora
+function combinations(obj) {
+    const keys = Object.keys(obj);
+    const values = Object.values(obj);
+
+    function gen(values, idx) {
+        if (idx === values.length) {
+            return [{}];  // Base case: return an array with an empty object
+        }
+
+        const current = values[idx];
+        const remaining = gen(values, idx + 1);  // recurse
+
+        const combos = [];
+        for (const item of current) {
+            for (const combination of remaining) {
+                combos.push({ ...combination, [keys[idx]]: item });  // Combine the current item with each of the combinations from the rest
+            }
+        }
+        return combos;
+    }
+
+    return gen(values, 0);
+}
+
+function fail(msg) {
+    // eslint-disable-next-line no-alert
+    alert(msg);
+  }
