7afd22521cc588103b835504344a7024c5e0f9f4
John Owens <jdowens@google.com>
2024-11-18 11:38:16 -0800

    convert to u32 input buffer , still giving me wonky results

--- stridedreadtest.mjs
+++ stridedreadtest.mjs
@@ -1,41 +1,53 @@
 import { range } from "./util.mjs";
 export const stridedReadTest = {
   category: "strided-read",
-  testname: "fp32-per-thread",
+  testname: "u32-per-thread",
   description:
     "Copies strided input array to output array. One thread is assigned per 32b input element.",
+  datatype: "u32",
   parameters: {
-    workgroupSize: range(0, 8).map((i) => 2 ** i),
-    log2stride: range(0, 12),
+    workgroupSize: [32, 64, 96, 128, 160, 192, 224, 256], // range(0, 8).map((i) => 2 ** i),
+    log2stride: range(/* 0 */ 11, 12),
   },
   trials: 10,
   /**
-   * Algorithm for ensuring strided access:
+   * First algorithm for ensuring strided access:
    * For stride 2**n: swap bit positions [0:n-1] and [n:2n-1] in the address
    * Bit mask = (1<<n)-1
    * xxxxaaabbb -> xxxxbbbaaa (for log2stride = 3)
    * Assumes that bitmath is free (compared to mem access time)
+   * This turns out to be susceptible to caching
+   * (all accesses within 2 ** (2 * log2stride) are in a block), so instead:
+   *
+   * Rotate by log2stride:
+   * xxxxxxxbbb -> bbbxxxxxxx (for log2stride = 3)
    */
-  kernel: (param) => /* wgsl */ `
+  kernel: function (param) {
+    /* function because we need 'this' */
+    return /* wgsl */ `
     /* output */
-    @group(0) @binding(0) var<storage, read_write> memDest: array<f32>;
+    @group(0) @binding(0) var<storage, read_write> memDest: array<u32>;
     /* input */
-    @group(0) @binding(1) var<storage, read> memSrc: array<f32>;
+    @group(0) @binding(1) var<storage, read> memSrc: array<u32>;
 
     @compute @workgroup_size(${param.workgroupSize}) fn memcpyKernel(
       @builtin(global_invocation_id) id: vec3u,
       @builtin(num_workgroups) nwg: vec3u,
       @builtin(workgroup_id) wgid: vec3u) {
         let i: u32 = id.y * nwg.x * ${param.workgroupSize} + id.x;
-        let low_mask: u32 = (1 << ${param.log2stride}) - 1;
-        let high_mask: u32 = low_mask << ${param.log2stride};
-        let total_mask: u32 = low_mask | high_mask;
-        let low_chunk: u32 = low_mask & i;
-        let high_chunk: u32 = high_mask & i;
-        let src: u32 = (i & ~total_mask) | (low_chunk << ${param.log2stride}) | (high_chunk >> ${param.log2stride});
-        memDest[i] = memSrc[src] + 1.0;
-    }`,
-  memsrcSize: (param) => 2 ** 27 / 4, // min(device.limits.maxBufferSize, maxStorageBufferBindingSize) / 4,
+        let address_space_mask: u32 = (1 << ${this.log2memsrcSize}) - 1;
+        /* assert: i == i & address_space_mask */
+        let lshift: u32 = ${param.log2stride};
+        let rshift: u32 = ${this.log2memsrcSize - param.log2stride};
+        let src: u32 = ((i << lshift) | (i >> rshift)) & address_space_mask;
+        memDest[i] = memSrc[src];// + 1;
+        // memDest[i] = src;
+    }`;
+  },
+  log2memsrcSize: 28,
+  memsrcSize: function (param) {
+    return 2 ** this.log2memsrcSize;
+  }, // min(device.limits.maxBufferSize, maxStorageBufferBindingSize) / 4,
   memdestSize: function (param) {
     /* need 'function', arrow notation has no 'this' */
     return this.memsrcSize(param);
@@ -44,7 +56,7 @@ export const stridedReadTest = {
     return this.memdestSize(param) / param.workgroupSize;
   },
   validate: (input, output) => {
-    return input + 1.0 == output;
+    return input /* + 1.0 */ == output;
   },
   bytesTransferred: (memInput, memOutput) => {
     /* assumes that strided access time >> coalesced writeback */
@@ -62,20 +74,7 @@ export const stridedReadTest = {
         label: "Stride distance",
       },
       caption:
-        "Strided memory bandwidth test, 1 fp32 per thread (lines are stride distance)",
-    },
-    {
-      x: {
-        field: (d) => d.param.workgroupSize,
-        label: "Workgroup size",
-      },
-      y: { field: "time", label: "Runtime (ns)" },
-      stroke: {
-        field: (d) => 2 ** d.param.log2stride,
-        label: "Stride distance",
-      },
-      caption:
-        "Strided memory bandwidth test, 1 fp32 per thread (lines are stride distance)",
+        "Strided memory bandwidth test, 1 u32 per thread (lines are stride distance)",
     },
     {
       x: {
@@ -90,7 +89,20 @@ export const stridedReadTest = {
         label: "Stride distance",
       },
       caption:
-        "Strided memory bandwidth test, 1 fp32 per thread (lines are stride distance)",
+        "Strided memory bandwidth test, 1 u32 per thread (lines are stride distance)",
+    },
+    {
+      x: {
+        field: (d) => d.param.workgroupSize,
+        label: "Workgroup size",
+      },
+      y: { field: "time", label: "Runtime (ns)" },
+      stroke: {
+        field: (d) => 2 ** d.param.log2stride,
+        label: "Stride distance",
+      },
+      caption:
+        "Strided memory bandwidth test, 1 u32 per thread (lines are stride distance)",
     },
   ],
 };
