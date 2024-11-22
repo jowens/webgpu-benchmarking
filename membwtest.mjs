556cd9d6c7861a51b9dc15494936a02b7b4fe0c0
John Owens <jdowens@google.com>
2024-11-22 10:09:10 -0800

    simple memsrc cpy works

--- membwtest.mjs
+++ membwtest.mjs
@@ -1,66 +1,75 @@
 import { range } from "./util.mjs";
 import { BaseTest } from "./basetest.mjs";
 class BaseMembwTest extends BaseTest {
-  category = "membw";
-  trials = 10;
-  validate = (input, output) => {
-    return input + 1.0 == output;
-  };
-  bytesTransferred = (memInput, memOutput) => {
-    return memInput.byteLength + memOutput.byteLength;
-  };
+  constructor(params) {
+    super(params); // writes parameters into this class
+    this.category = "membw";
+    this.trials = 10;
+    this.validate = (input, output) => {
+      return input + 1.0 == output;
+    };
+    this.bytesTransferred = (memInput, memOutput) => {
+      return memInput.byteLength + memOutput.byteLength;
+    };
+  }
 }
 
-export class MembwSimpleTest extends BaseMembwTest {
-  testname = "fp32-per-thread";
-  description =
-    "Copies input array to output array. One thread is assigned per 32b input element.";
-  parameters = {
-    workgroupSize: range(0, 7).map((i) => 2 ** i),
-    memsrcSize: range(10, 25).map((i) => 2 ** i),
-  };
-  kernel = (param) => /* wgsl */ `
-    /* output */
-    @group(0) @binding(0) var<storage, read_write> memDest: array<f32>;
-    /* input */
-    @group(0) @binding(1) var<storage, read> memSrc: array<f32>;
+const MembwSimpleTestParams = {
+  workgroupSize: range(0, 7).map((i) => 2 ** i),
+  memsrcSize: range(10, 25).map((i) => 2 ** i),
+};
 
-    @compute @workgroup_size(${param.workgroupSize}) fn memcpyKernel(
-      @builtin(global_invocation_id) id: vec3u,
-      @builtin(num_workgroups) nwg: vec3u,
-      @builtin(workgroup_id) wgid: vec3u) {
-        let i = id.y * nwg.x * ${param.workgroupSize} + id.x;
-        memDest[i] = memSrc[i] + 1.0;
-    }`;
-  plots = [
+export class MembwSimpleTestClass extends BaseMembwTest {
+  constructor(params) {
+    super(params);
+    this.testname = "fp32-per-thread";
+    this.description =
+      "Copies input array to output array. One thread is assigned per 32b input element.";
+    this.kernel = (param) => /* wgsl */ `
+      /* output */
+      @group(0) @binding(0) var<storage, read_write> memDest: array<f32>;
+      /* input */
+      @group(0) @binding(1) var<storage, read> memSrc: array<f32>;
+
+      @compute @workgroup_size(${this.workgroupSize}) fn memcpyKernel(
+        @builtin(global_invocation_id) id: vec3u,
+        @builtin(num_workgroups) nwg: vec3u,
+        @builtin(workgroup_id) wgid: vec3u) {
+          let i = id.y * nwg.x * ${this.workgroupSize} + id.x;
+          memDest[i] = memSrc[i] + 1.0;
+      }`;
+    this.memdestSize = this.memsrcSize;
+    this.workgroupCount = this.memsrcSize / this.workgroupSize;
+  }
+  static plots = [
     {
-      x: { field: (d) => d.param.memsrcSize, label: "Copied array size (B)" },
+      x: { field: "memsrcSize", label: "Copied array size (B)" },
       y: { field: "bandwidth", label: "Achieved bandwidth (GB/s)" },
-      stroke: { field: (d) => d.param.workgroupSize },
+      stroke: { field: "workgroupSize" },
       caption:
         "Memory bandwidth test, 1 fp32 per thread (lines are workgroup size)",
     },
     {
-      x: { field: (d) => d.param.memsrcSize, label: "Copied array size (B)" },
+      x: { field: "memsrcSize", label: "Copied array size (B)" },
       y: {
         field: "bandwidthCPU",
         label: "Achieved bandwidth (GB/s) [CPU measurement]",
       },
-      stroke: { field: (d) => d.param.workgroupSize },
+      stroke: { field: "workgroupSize" },
       caption:
         "Memory bandwidth test, 1 fp32 per thread (lines are workgroup size)",
     },
     {
       x: { field: "time", label: "GPU time (ns)" },
       y: { field: "cpuns", label: "CPU time (ns)" },
-      stroke: { field: (d) => d.param.workgroupSize },
+      stroke: { field: "workgroupSize" },
       caption:
         "Memory bandwidth test, 1 fp32 per thread (lines are workgroup size)",
     },
     {
-      x: { field: (d) => d.param.memsrcSize, label: "Copied array size (B)" },
+      x: { field: "memsrcSize", label: "Copied array size (B)" },
       y: { field: "cpugpuDelta", label: "CPU - GPU time (ns)" },
-      stroke: { field: (d) => d.param.workgroupSize },
+      stroke: { field: "workgroupSize" },
       caption:
         "Memory bandwidth test, 1 fp32 per thread (lines are workgroup size)",
     },
@@ -71,34 +80,39 @@ export class MembwSimpleTest extends BaseMembwTest {
  * grid stride loop, now we don't assign a fixed number of elements per thread
  * background: https://developer.nvidia.com/blog/cuda-pro-tip-write-flexible-kernels-grid-stride-loops/
  */
-export class MembwGSLTest extends BaseMembwTest {
-  testname = "GSL fp32-per-thread";
-  kernel = (param) => /* wgsl */ `
-  /* output */
-  @group(0) @binding(0) var<storage, read_write> memDest: array<f32>;
-  /* input */
-  @group(0) @binding(1) var<storage, read> memSrc: array<f32>;
 
-  @compute @workgroup_size(${param.workgroupSize}) fn memcpyKernel(
-    @builtin(global_invocation_id) id: vec3u,
-    @builtin(num_workgroups) nwg: vec3u, // == dispatch
-    @builtin(workgroup_id) wgid: vec3u) {
-      /* grid-stride loop: assume nwg.y == 1 */
-      for (var i = id.x;
-           i < arrayLength(&memSrc);
-           i += nwg.x * ${param.workgroupSize}) {
-        memDest[i] = memSrc[i] + 1.0;
-      }
-  }`;
-  dispatchGeometry = (param) => {
-    return [param.workgroupCount];
-  };
-  parameters = {
-    workgroupSize: range(0, 7).map((i) => 2 ** i),
-    memsrcSize: range(10, 25).map((i) => 2 ** i),
-    workgroupCount: range(5, 10).map((i) => 2 ** i),
-  };
-  plots = [
+const MembwGSLTestParams = {
+  workgroupSize: range(0, 7).map((i) => 2 ** i),
+  memsrcSize: range(10, 25).map((i) => 2 ** i),
+  workgroupCount: range(5, 10).map((i) => 2 ** i),
+};
+export class MembwGSLTestClass extends BaseMembwTest {
+  constructor(params) {
+    super(params);
+    this.testname = "GSL fp32-per-thread";
+    this.kernel = (param) => /* wgsl */ `
+      /* output */
+      @group(0) @binding(0) var<storage, read_write> memDest: array<f32>;
+      /* input */
+      @group(0) @binding(1) var<storage, read> memSrc: array<f32>;
+
+      @compute @workgroup_size(${param.workgroupSize}) fn memcpyKernel(
+        @builtin(global_invocation_id) id: vec3u,
+        @builtin(num_workgroups) nwg: vec3u, // == dispatch
+        @builtin(workgroup_id) wgid: vec3u) {
+          /* grid-stride loop: assume nwg.y == 1 */
+          for (var i = id.x;
+               i < arrayLength(&memSrc);
+               i += nwg.x * ${param.workgroupSize}) {
+            memDest[i] = memSrc[i] + 1.0;
+          }
+      }`;
+    this.dispatchGeometry = (param) => {
+      return [param.workgroupCount];
+    };
+  }
+
+  static plots = [
     {
       x: { field: (d) => d.param.memsrcSize, label: "Copied array size (B)" },
       y: { field: "bandwidth", label: "Achieved bandwidth (GB/s)" },
@@ -137,3 +151,13 @@ export class MembwAdditionalPlots extends BaseMembwTest {
     },
   ];
 }
+
+export const MembwSimpleTestSuite = {
+  class: MembwSimpleTestClass,
+  params: MembwSimpleTestParams,
+};
+
+export const MembwGSLTestSuite = {
+  class: MembwGSLTestClass,
+  params: MembwGSLTestParams,
+};
