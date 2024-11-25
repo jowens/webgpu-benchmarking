75be3db9e268d5bfc87201e98e85e8519f3ff8ba
John Owens <jdowens@google.com>
2024-11-25 14:29:40 -0800

    global atomic reduce only, appears to work

--- reduce.mjs
+++ reduce.mjs
@@ -5,28 +5,61 @@ class BaseReduceTest extends BaseTest {
   trials = 10;
 }
 
-export class ReducePerWGTest extends BaseReduceTest {
-  testname = "reduce per wg";
-  workgroupSizes = range(2, 7).map((i) => 2 ** i);
-  memsrcSizes = range(16, 17).map((i) => 2 ** i);
-  kernel = (workgroupSize) => /* wgsl */ `
-    enable subgroups;
-    // var<workgroup> sum: f32; // zero initialized?
-    /* output */
-    @group(0) @binding(0) var<storage, read_write> memDest: array<f32>;
-    /* input */
-    @group(0) @binding(1) var<storage, read> memSrc: array<f32>;
+const AtomicGlobalU32ReduceTestParams = {
+  workgroupSize: range(2, 7).map((i) => 2 ** i),
+  workgroupCount: range(5, 15).map((i) => 2 ** i),
+};
 
-    @compute @workgroup_size(${workgroupSize}) fn reducePerWGKernel(
-      @builtin(global_invocation_id) id: vec3u,
-      @builtin(num_workgroups) nwg: vec3u,
-      @builtin(workgroup_id) wgid: vec3u) {
-        let i = id.y * nwg.x * ${workgroupSize} + id.x;
-        let sa = subgroupAdd(memSrc[i]);
-        memDest[i] = sa;
+class AtomicGlobalU32ReduceTestClass extends BaseReduceTest {
+  constructor(params) {
+    super(params);
+    this.testname = "atomic u32 sum reduction, should be very slow";
+    this.datatype = "u32";
+    this.memsrcSize = this.workgroupSize * this.workgroupCount;
+    this.memdestSize = 1;
+    this.bytesTransferred = (this.memsrcSize + this.memdestSize) * 4;
+    this.kernel = () => /* wgsl */ `
+      enable subgroups;
+      /* output */
+      @group(0) @binding(0) var<storage, read_write> memDest: atomic<u32>;
+      /* input */
+      @group(0) @binding(1) var<storage, read> memSrc: array<u32>;
+
+      @compute @workgroup_size(${this.workgroupSize}) fn reducePerWGKernel(
+        @builtin(global_invocation_id) id: vec3u,
+        @builtin(num_workgroups) nwg: vec3u) {
+          let i = id.y * nwg.x * ${this.workgroupSize} + id.x;
+          atomicAdd(&memDest, memSrc[i]);
+      }`;
+  }
+  validate = (memsrc, memdest) => {
+    const sum = new Uint32Array([0]);
+    for (let i = 0; i < memsrc.length; sum[0] += memsrc[i++]);
+    if (memdest[0] != sum[0]) {
+      return `Element ${0}: expected ${sum[0]}, instead saw ${memdest[0]}.`;
+    } else {
+      return "";
     }
-  `;
-  validate = (f) => {
-    return f;
   };
+  static plots = [
+    {
+      x: { field: "memsrcSize", label: "Input array size (B)" },
+      y: { field: "bandwidth", label: "Achieved bandwidth (GB/s)" },
+      stroke: { field: "workgroupSize" },
+      caption:
+        "Atomic global reduction, 1 u32 per thread (lines are workgroup size)",
+    },
+    {
+      x: { field: "memsrcSize", label: "Input array size (B)" },
+      y: { field: "bandwidth", label: "Achieved bandwidth (GB/s)" },
+      stroke: { field: "workgroupCount" },
+      caption:
+        "Atomic global reduction, 1 u32 per thread (lines are workgroup count)",
+    },
+  ];
 }
+
+export const AtomicGlobalU32ReduceTestSuite = {
+  class: AtomicGlobalU32ReduceTestClass,
+  params: AtomicGlobalU32ReduceTestParams,
+};
