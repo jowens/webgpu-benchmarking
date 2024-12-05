93d9772ba17b238270f9d7e4bc8d2ba20abfc49f
John Owens <jdowens@google.com>
2024-12-05 14:22:30 -0800

    got rid of some defaults

--- binop.mjs
+++ binop.mjs
@@ -2,14 +2,8 @@
 
 class BinOp {
   constructor(args) {
-    // defaults are BinOpAdd
-    this.datatype = args.datatype ?? "u32";
-    this.identity = args.identity ?? 0;
-    this.op = args.op ?? ((a, b) => a + b);
-    this.wgslfn =
-      args.wgslfn ??
-      `fn binop(a : ${this.datatype}, b : ${this.datatype}) -> ${this.datatype} {return a+b;}`;
-    this.wgslatomic = args.wgslatomic ?? "atomicAdd";
+    // no defaults! if something is undefined, go with it
+    Object.assign(this, args);
   }
 }
 
@@ -27,10 +21,8 @@ class BinOpAdd extends BinOp {
         this.wgslatomic = "atomicAdd"; // u32 only
         break;
     }
-    this.wgslfn =
-      args.wgslfn ??
-      `fn binop(a : ${this.datatype}, b : ${this.datatype}) -> ${this.datatype} {return a+b;}`;
-    this.op = args.op ?? ((a, b) => a + b);
+    this.wgslfn = `fn binop(a : ${this.datatype}, b : ${this.datatype}) -> ${this.datatype} {return a+b;}`;
+    this.op = (a, b) => a + b;
   }
 }
 
@@ -63,3 +55,42 @@ class BinOpMin extends BinOp {
 export const BinOpMinU32 = new BinOpMin({ datatype: "u32" });
 export const BinOpMinF32 = new BinOpMin({ datatype: "f32" });
 export const BinOpMinI32 = new BinOpMin({ datatype: "i32" });
+
+class BinOpMax extends BinOp {
+  constructor(args) {
+    super(args);
+    /* identity depends on datatype */
+    switch (this.datatype) {
+      case "f32":
+        this.identity = -3.40282346638529e38; // -FLT_MAX
+        break;
+      case "i32":
+        this.identity = 0xf0000000;
+        break;
+      case "u32": // fall-through OK
+      default:
+        this.identity = 0;
+        break;
+    }
+    this.op = (a, b) => Math.max(a, b);
+    this.wgslfn = `fn binop(a : ${this.datatype}, b : ${this.datatype}) -> ${this.datatype} {return max(a,b);}`;
+    this.wgslatomic = "atomicMax";
+  }
+}
+
+export const BinOpMaxU32 = new BinOpMax({ datatype: "u32" });
+export const BinOpMaxF32 = new BinOpMax({ datatype: "f32" });
+export const BinOpMaxI32 = new BinOpMax({ datatype: "i32" });
+
+class BinOpMultiply extends BinOp {
+  constructor(args) {
+    super(args);
+    this.identity = 1;
+    this.op = (a, b) => a * b;
+    this.wgslfn = `fn binop(a : ${this.datatype}, b : ${this.datatype}) -> ${this.datatype} {return a*b;}`;
+  }
+}
+
+export const BinOpMultiplyU32 = new BinOpMultiply({ datatype: "u32" });
+export const BinOpMultiplyF32 = new BinOpMultiply({ datatype: "f32" });
+export const BinOpMultiplyI32 = new BinOpMultiply({ datatype: "i32" });
