c23a338be36f13ed4ec06c54de6d98e2f402b2b5
John Owens <jdowens@google.com>
2024-12-04 16:10:43 -0800

    kernel -> set of operations, supports kernel and initialized memory block now

--- basetest.mjs
+++ basetest.mjs
@@ -26,48 +26,80 @@ export class BasePrimitive {
     );
   }
   async execute() {
-    const computeModule = this.device.createShaderModule({
-      label: `module: ${this.constructor.name}`,
-      code: this.kernel(),
-    });
-
-    const kernelPipeline = this.device.createComputePipeline({
-      label: `${this.constructor.name} compute pipeline`,
-      layout: "auto",
-      compute: {
-        module: computeModule,
-      },
-    });
-
-    const bindings = [];
-    for (const [index, binding] of Object.entries(this.bindings)) {
-      bindings.push({
-        binding: index,
-        resource: { buffer: binding },
-      });
-    }
-
-    const kernelBindGroup = this.device.createBindGroup({
-      label: `bindGroup for ${this.constructor.name} kernel`,
-      layout: kernelPipeline.getBindGroupLayout(0),
-      entries: bindings,
-    });
-
     const encoder = this.device.createCommandEncoder({
       label: `${this.constructor.name} primitive encoder`,
     });
 
-    const kernelPass = encoder.beginComputePass(encoder, {
-      label: `${this.constructor.name} compute pass`,
-    });
+    for (const action of this.compute) {
+      switch (action.constructor) {
+        case Kernel:
+          const computeModule = this.device.createShaderModule({
+            label: `module: ${this.constructor.name}`,
+            code: action.kernel(),
+          });
 
-    kernelPass.setPipeline(kernelPipeline);
-    kernelPass.setBindGroup(0, kernelBindGroup);
-    kernelPass.dispatchWorkgroups(...this.getDispatch());
-    kernelPass.end();
+          const kernelPipeline = this.device.createComputePipeline({
+            label: `${this.constructor.name} compute pipeline`,
+            layout: "auto",
+            compute: {
+              module: computeModule,
+            },
+          });
+
+          const bindings = [];
+          for (const [index, binding] of Object.entries(this.bindings)) {
+            bindings.push({
+              binding: index,
+              resource: { buffer: binding },
+            });
+          }
+
+          const kernelBindGroup = this.device.createBindGroup({
+            label: `bindGroup for ${this.constructor.name} kernel`,
+            layout: kernelPipeline.getBindGroupLayout(0),
+            entries: bindings,
+          });
+
+          const kernelPass = encoder.beginComputePass(encoder, {
+            label: `${this.constructor.name} compute pass`,
+          });
+
+          kernelPass.setPipeline(kernelPipeline);
+          kernelPass.setBindGroup(0, kernelBindGroup);
+          kernelPass.dispatchWorkgroups(...this.getDispatch());
+          kernelPass.end();
+          break;
+        case InitializeMemoryBlock:
+          let InitClass;
+          switch (action.datatype) {
+            case "f32":
+              InitClass = Float32Array;
+              break;
+            case "i32":
+              InitClass = Int32Array;
+              break;
+            case "u32":
+            default:
+              InitClass = Uint32Array;
+              break;
+          }
+          /* initialize to action.value */
+          const initBlock = InitClass.from(
+            { length: action.buffer.size / InitClass.BYTES_PER_ELEMENT },
+            () => action.value
+          );
+          this.device.queue.writeBuffer(
+            action.buffer,
+            0 /* offset */,
+            initBlock
+          );
+          break;
+      }
+    }
 
     const commandBuffer = encoder.finish();
-    return this.device.queue.submit([commandBuffer]);
+    this.device.queue.submit([commandBuffer]);
+    await this.device.queue.onSubmittedWorkDone();
   }
 }
 
@@ -115,5 +147,19 @@ export class BaseTestSuite {
   }
   getPlots() {
     return this.processedPlots;
+  }
+}
+
+export class Kernel {
+  constructor(k) {
+    this.kernel = k;
+  }
+}
+
+export class InitializeMemoryBlock {
+  constructor(buffer, value, datatype) {
+    this.buffer = buffer;
+    this.value = value;
+    this.datatype = datatype;
   }
 }
