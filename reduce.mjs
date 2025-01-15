9bbef41f058340f5ebbe387571d59c0d750d70f2
John Owens <jdowens@google.com>
2025-01-15 14:08:51 -0800

    explicit workgroupReduce function is now dead code since it's moved to wgslFunctions

--- reduce.mjs
+++ reduce.mjs
@@ -187,43 +187,6 @@ export class NoAtomicPKReduce extends BaseReduce {
         return (a + b - 1) / b;
       }
 
-      fn workgroupReduceX(id: vec3u,
-                         nwg: vec3u,
-                         lid: u32,
-                         sgsz : u32) -> ${this.datatype} {
-        /* TODO: fix 'assume id.y == 0 always' */
-        /* TODO: what if there are more threads than subgroup_size * subgroup_size? */
-        var acc: ${this.datatype} = ${this.binop.identity};
-        var numSubgroups = roundUpDivU32(${this.workgroupSize}, sgsz);
-        /* note: this access pattern is not particularly TLB-friendly */
-        for (var i = id.x;
-          i < arrayLength(&inputBuffer);
-          i += nwg.x * ${this.workgroupSize}) {
-            /* on every iteration, grab wkgpsz items */
-            acc = binop(acc, inputBuffer[i]);
-        }
-        /* acc contains a partial sum for every thread */
-        workgroupBarrier();
-        /* now we need to reduce acc within our workgroup */
-        /* switch to local IDs only. write into wg memory */
-        acc = ${this.binop.subgroupOp}(acc);
-        var mySubgroupID = lid / sgsz;
-        if (subgroupElect()) {
-          /* I'm the first element in my subgroup */
-          temp[mySubgroupID] = acc;
-        }
-        workgroupBarrier(); /* completely populate wg memory */
-        if (lid < sgsz) { /* only activate 0th subgroup */
-          /* read sums of all other subgroups into acc, in parallel across the subgroup */
-          /* acc is only valid for lid < numSubgroups, so ... */
-          /* select(f, t, cond) */
-          acc = select(${this.binop.identity}, temp[lid], lid < numSubgroups);
-        }
-        /* acc is called here for everyone, but it only matters for thread 0 */
-        acc = ${this.binop.subgroupOp}(acc);
-        return acc;
-      }
-
       ${BasePrimitive.fnDeclarations.workgroupReduce(this, {
         inputBuffer: "inputBuffer",
         temp: "temp",
