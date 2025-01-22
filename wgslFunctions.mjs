92bc074d15d43290b8c97fc3171672f5a1b9a9ca
John Owens <jdowens@google.com>
2025-01-22 12:07:35 -0800

    workgroup exclusive scan works although there's a weird -0 that's output as the first element of the scan (identity)

--- wgslFunctions.mjs
+++ wgslFunctions.mjs
@@ -23,7 +23,7 @@ export const wgslFunctions = {
     workgroupBarrier();
     /* now we need to reduce acc within our workgroup */
     /* switch to local IDs only. write into wg memory */
-    acc = ${env.binop.subgroupOp}(acc);
+    acc = ${env.binop.subgroupReduceOp}(acc);
     var mySubgroupID = lid / sgsz;
     if (subgroupElect()) {
       /* I'm the first element in my subgroup */
@@ -37,8 +37,49 @@ export const wgslFunctions = {
       acc = select(${env.binop.identity}, ${bindings.temp}[lid], lid < numSubgroups);
     }
     /* acc is called here for everyone, but it only matters for thread 0 */
-    acc = ${env.binop.subgroupOp}(acc);
+    acc = ${env.binop.subgroupReduceOp}(acc);
     return acc;
         };`;
+  },
+  workgroupExclusiveScan: (
+    env,
+    bindings = { inputBuffer: "inputBuffer", temp: "temp" }
+  ) => {
+    return /* wgsl */ `
+    fn workgroupExclusiveScan(id: vec3u,
+      nwg: vec3u,
+      lid: u32,
+      sgsz: u32) -> ${env.datatype} {
+        /* TODO: fix 'assume id.y == 0 always' */
+    /* TODO: what if there are more threads than subgroup_size * subgroup_size? */
+    var numSubgroups = roundUpDivU32(${env.workgroupSize}, sgsz);
+    var i = id.x;
+    var in = select(${env.binop.identity}, ${bindings.inputBuffer}[i], i < arrayLength(&${bindings.inputBuffer}));
+    workgroupBarrier();
+    /* "in" now contains the block of data to scan, padded with the identity */
+    /* (1) reduce "in" within our workgroup */
+    /* switch to local IDs only. write into wg memory */
+    var sgReduction = ${env.binop.subgroupReduceOp}(in);
+    var mySubgroupID = lid / sgsz;
+    if (subgroupElect()) {
+      /* I'm the first element in my subgroup */
+      ${bindings.temp}[mySubgroupID] = sgReduction;
+    }
+    workgroupBarrier(); /* completely populate wg memory */
+    /* Now temp[i] contains reduction of subgroup i */
+    /* (2) read sums of all other subgroups into acc, in parallel across the subgroup */
+    /** acc is only valid for lid < numSubgroups, but we need uniform control flow
+     * for the subgroupScanOp */
+    var spineScanInput = select(${env.binop.identity}, ${bindings.temp}[lid], lid < numSubgroups);
+    var spineScanOutput = ${env.binop.subgroupExclusiveScanOp}(spineScanInput);
+    if (lid < sgsz) { /* only activate 0th subgroup */
+      ${bindings.temp}[lid] = spineScanOutput;
+    }
+    workgroupBarrier();
+    /* now go add that spineScan value back to my local scan */
+    var subgroupExclusiveScan = ${env.binop.subgroupExclusiveScanOp}(in);
+    return binop(${bindings.temp}[mySubgroupID], subgroupExclusiveScan);
+    // return ${bindings.temp}[mySubgroupID];
+  };`;
   },
 };
