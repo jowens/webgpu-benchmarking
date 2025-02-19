371c1b32f6a90c7a49613e4bdffcd4f665df1d7c
John Owens <jdowens@google.com>
2025-02-19 15:41:32 -0800

    handles file: too

--- benchmarking_chrome.mjs
+++ benchmarking_chrome.mjs
@@ -1,9 +1,11 @@
 const isLocalhost =
   window.location.hostname === "localhost" ||
-  window.location.hostname === "127.0.0.1";
+  window.location.hostname === "127.0.0.1" ||
+  window.location.protocol === "file:";
+
 const modulePath =
-  (isLocalhost ? "http://localhost:8000/" : "https://jowens.github.io/") +
-  "webgpu-benchmarking/benchmarking.mjs";
+  (isLocalhost ? "http://localhost:8000" : "https://jowens.github.io") +
+  "/webgpu-benchmarking/benchmarking.mjs";
 
 import(modulePath)
   .then(({ main }) => {
