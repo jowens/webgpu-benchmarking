413bb43e5e3caf22c47c25134728cd9e6ca489e9
John Owens <jdowens@google.com>
2025-02-19 10:05:22 -0800

    fleshed out shuffle regression, no validation yet

--- subgroupRegression.mjs
+++ subgroupRegression.mjs
@@ -13,46 +13,59 @@ export class SubgroupShuffleRegression extends BasePrimitive {
   constructor(args) {
     super(args);
     this.getDispatchGeometry = this.getSimpleDispatchGeometry;
+    this.knownBuffers = ["inputBuffer", "outputBuffer"];
+  }
+  get bytesTransferred() {
+    return (
+      this.getBuffer("inputBuffer").size + this.getBuffer("outputBuffer").size
+    );
   }
   kernel = () => {
     return /* wgsl */ `
 ${this.fnDeclarations.enableSubgroupsIfAppropriate}
+
 @group(0) @binding(0)
-var<storage, read> inputBuffer: array<${this.datatype}>;
+var<storage, read_write> outputBuffer: array<${this.datatype}>;
 
 @group(0) @binding(1)
-var<storage, read> outputBuffer: array<${this.datatype}>;
+var<storage, read> inputBuffer: array<${this.datatype}>;
 
 ${this.fnDeclarations.subgroupEmulation}
 /* defines "binop", the operation associated with the scan monoid */
 ${this.binop.wgslfn}
 ${this.fnDeclarations.commonDefinitions}
-${this.fnDeclarations.computeLinearizedGridParameters}
-${this.fnDeclarations.subgroupBallot}
+${this.fnDeclarations.subgroupShuffle}
 
 @compute @workgroup_size(${this.workgroupSize}, 1, 1)
 fn main(builtinsUniform: BuiltinsUniform,
         builtinsNonuniform: BuiltinsNonuniform) {
   ${this.fnDeclarations.initializeSubgroupVars}
-  ${this.fnDeclarations.computeLinearizedGridParameters}
+  ${this.fnDeclarations.computeLinearizedGridParametersSplit}
   outputBuffer[gid] = subgroupShuffle(inputBuffer[gid], gid ^ 1);
 }`;
   };
-  finalizeRuntimeParameters() {}
+  finalizeRuntimeParameters() {
+    const inputSize = this.getBuffer("inputBuffer").size; // bytes
+    const inputLength = inputSize / 4; /* 4 is size of datatype */
+    this.workgroupCount = Math.ceil(inputLength / this.workgroupSize);
+  }
   compute() {
     this.finalizeRuntimeParameters();
     return [
       new Kernel({
         kernel: this.kernel,
-        bufferTypes: [["read-only-storage", "storage"]],
-        bindings: [["inputBuffer", "outputBuffer"]],
+        bufferTypes: [["storage", "read-only-storage"]],
+        bindings: [["outputBuffer", "inputBuffer"]],
+        logKernelCodeToConsole: true,
       }),
     ];
   }
 }
 
 const SubgroupParams = {
-  inputLength: range(8, 28).map((i) => 2 ** i),
+  inputLength: range(8, 10).map((i) => 2 ** i),
+  workgroupSize: range(5, 8).map((i) => 2 ** i),
+  datatype: ["f32"],
 };
 
 export const SubgroupShuffleTestSuite = new BaseTestSuite({
@@ -60,7 +73,6 @@ export const SubgroupShuffleTestSuite = new BaseTestSuite({
   testSuite: "subgroupShuffle",
   trials: 1,
   params: SubgroupParams,
-  uniqueRuns: ["inputLength", "workgroupSize"],
   primitive: SubgroupShuffleRegression,
   primitiveConfig: {
     datatype: "u32",
