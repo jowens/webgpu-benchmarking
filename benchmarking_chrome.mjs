990a20d5006d34a5bb5c72484d793d2e66ff67ed
John Owens <jdowens@google.com>
2025-02-19 15:14:44 -0800

    ok let's try a more substantial HTML wrapper that should work as a file, from localhost, and from github

--- benchmarking_chrome.mjs
+++ benchmarking_chrome.mjs
@@ -1,3 +1,17 @@
-import { main } from "http://localhost:8000/webgpu-benchmarking/benchmarking.mjs";
+const isLocalhost =
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1";
+const modulePath =
+  (isLocalhost ? "http://localhost:8000/" : "https://jowens.github.io/") +
+  "webgpu-benchmarking/benchmarking.mjs";
 
-main(navigator);
+import(modulePath)
+  .then(({ main }) => {
+    main(navigator);
+  })
+  .catch((error) => {
+    console.error("Error loading module", error);
+  });
+
+//import { main } from "http://localhost:8000/webgpu-benchmarking/benchmarking.mjs";
+// main(navigator);
