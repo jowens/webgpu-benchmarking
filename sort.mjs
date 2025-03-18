a5131bdaf61b018aefbfd31494c285150521ce51
John Owens <jdowens@google.com>
2025-03-18 13:27:04 -0700

    moved buffer management, bytesTransferred() to subclass

--- sort.mjs
+++ sort.mjs
@@ -4,21 +4,9 @@ export class BaseSort extends BasePrimitive {
   constructor(args) {
     super(args);
 
-    this.knownBuffers = ["keysIn", "keysOut", "payloadIn", "payloadOut"];
-
-    for (const knownBuffer of this.knownBuffers) {
-      /* we passed an existing buffer into the constructor */
-      if (knownBuffer in args) {
-        this.registerBuffer({ label: knownBuffer, buffer: args[knownBuffer] });
-        delete this[knownBuffer]; // let's make sure it's in one place only
-      }
-    }
+    /* buffer registration should be done in subclass */
 
     /* by default, delegate to simple call from BasePrimitive */
     this.getDispatchGeometry = this.getSimpleDispatchGeometry;
-  }
-
-  get bytesTransferred() {
-    return this.getBuffer("keysIn").size + this.getBuffer("keysOut").size;
   }
 }
