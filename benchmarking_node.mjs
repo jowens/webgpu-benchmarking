291c8b58dea83d45f22e45f92d1fb6faccb260d8
John Owens <jdowens@google.com>
2025-03-27 17:08:39 -0700

    using webgpu package now

--- benchmarking_node.mjs
+++ benchmarking_node.mjs
@@ -1,11 +1,7 @@
-"use strict";
-import { createRequire } from "node:module";
-const require = createRequire(import.meta.url);
-const dawn = require("../../src/dawn-build/dawn.node");
-Object.assign(globalThis, dawn.globals); // Provides constants like GPUBufferUsage.MAP_READ
-
-let navigator = {
-  gpu: dawn.create(["enable-dawn-features=use_user_defined_labels_in_backend"]),
+import { globals, create } from "webgpu";
+Object.assign(globalThis, globals);
+const navigator = {
+  gpu: create(["enable-dawn-features=use_user_defined_labels_in_backend"]),
 };
 
 import { main } from "./benchmarking.mjs";
