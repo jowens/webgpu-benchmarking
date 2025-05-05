e837ccfc2543e4dbb4b5b8c8b3b043ba839f27b4
John Owens <jdowens@google.com>
2025-05-05 10:44:47 -0700

    attempt to lock in GPU on my Windows machine

--- benchmarking_node.mjs
+++ benchmarking_node.mjs
@@ -1,7 +1,15 @@
 import { globals, create } from "webgpu";
 Object.assign(globalThis, globals);
+const gpuCreateFeatures = [
+  "enable-dawn-features=use_user_defined_labels_in_backend",
+];
+var os = import("os");
+if (process.platform === "win32" && os.hostname() === "jdowens2") {
+  /* cannot figure out how to make Windows pick my discrete GPU */
+  gpuCreateFeatures.push("adapter=NVIDIA RTX 4000 Ada Generation Laptop GPU");
+}
 const navigator = {
-  gpu: create(["enable-dawn-features=use_user_defined_labels_in_backend"]),
+  gpu: create(gpuCreateFeatures),
 };
 
 import { main } from "./benchmarking.mjs";
