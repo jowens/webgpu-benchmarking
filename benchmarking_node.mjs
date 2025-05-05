a6798e745cc0596d9ac9bf95b00c7974b09421bf
John Owens <jdowens@google.com>
2025-05-05 10:50:55 -0700

    take 2 on import os/hostname

--- benchmarking_node.mjs
+++ benchmarking_node.mjs
@@ -1,10 +1,10 @@
 import { globals, create } from "webgpu";
+import { hostname } from "os";
 Object.assign(globalThis, globals);
 const gpuCreateFeatures = [
   "enable-dawn-features=use_user_defined_labels_in_backend",
 ];
-var os = import("os");
-if (process.platform === "win32" && os.hostname() === "jdowens2") {
+if (process.platform === "win32" && hostname() === "jdowens2") {
   /* cannot figure out how to make Windows pick my discrete GPU */
   gpuCreateFeatures.push("adapter=NVIDIA RTX 4000 Ada Generation Laptop GPU");
 }
