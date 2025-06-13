52779293a6c47fe1167fed4a9374b778684f8acc
John Owens <jdowens@google.com>
2025-06-13 08:19:23 -0700

    added enabled options and code

--- mapvariants.mjs
+++ mapvariants.mjs
@@ -2,13 +2,22 @@ export class CountingMap {
   #hits = 0; // Private class field for hits
   #misses = 0; // Private class field for misses
   #map; // Private class field for the internal Map
+  #enabled;
 
-  constructor(iterable) {
+  constructor({ iterable, enabled }) {
     this.#map = new Map(iterable);
+    if (enabled) {
+      this.enable();
+    } else {
+      this.disable();
+    }
   }
 
   // Override the get() method to count hits and misses
   get(key) {
+    if (this.disabled) {
+      return undefined;
+    }
     if (this.#map.has(key)) {
       this.#hits++; // Accessing private field
       return this.#map.get(key);
@@ -20,11 +29,15 @@ export class CountingMap {
 
   // Delegate other Map methods to the internal #map
   set(key, value) {
-    return this.#map.set(key, value);
+    if (this.enabled) {
+      return this.#map.set(key, value);
+    } else {
+      return undefined;
+    }
   }
 
   has(key) {
-    return this.#map.has(key);
+    return this.enabled && this.#map.has(key);
   }
 
   delete(key) {
@@ -48,6 +61,22 @@ export class CountingMap {
 
   get misses() {
     return this.#misses;
+  }
+
+  enable() {
+    this.#enabled = true;
+  }
+
+  disable() {
+    this.#enabled = false;
+  }
+
+  get enabled() {
+    return this.#enabled;
+  }
+
+  get disabled() {
+    return !this.#enabled;
   }
 
   // Iteration methods (delegate to the internal #map's iterators)
